f9137c118d5a519a6d86be1c5869295ab6f9bc4c
Emanuil Nikolov <enikolov.intl@gmail.com>
2018-02-26 19:30:41 +0200

    Added more exercises.

---
+++ 04. PHP_Functions/Fibonacci_Matrix.php
@@ -0,0 +1,29 @@
+<?php
+$input = array_map("intval", explode(" ", trim(fgets(STDIN))));
+
+$zeroArr = array_fill(0, count($input), $input[0]);
+
+$firstArr = function (array $arr) use ($zeroArr) {
+    if (count($arr) > 2) {
+        $lastTwo = array();
+        foreach ($arr as $num) {
+            $lastTwo[] = $zeroArr[0] + $num;
+        }
+        $prevArr = $arr;
+        $columnSum = array();
+        for ($l = 0; $l < count($arr) - 3; ++$l) {
+            $columnSum = array();
+            for ($i = 0; $i < count($arr); ++$i) {
+                for ($n = 0; $n < 1; ++$n) {
+                    $columnSum[] = $lastTwo[$i] + $prevArr[$i];
+                }
+                $prevArr[$i] = $lastTwo[$i];
+                $lastTwo[$i] = $columnSum[$i];
+            }
+
+        }
+        return $columnSum;
+    }
+    return $arr;
+};
+var_dump($firstArr($input));
